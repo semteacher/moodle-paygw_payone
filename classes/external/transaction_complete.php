b6d0de1f237d43b7805174af116d15f3a27cae3d
georgmaisser <georg.maisser@wunderbyte.at>
2022-04-18 12:30:20 +0200

    More information for payment_error event.

--- classes/external/transaction_complete.php
+++ classes/external/transaction_complete.php
@@ -170,7 +170,10 @@ class transaction_complete extends external_api {
             $context = context_system::instance();
             $event = payment_error::create(array('context' => $context, 'other' => [
                     'message' => $message,
-                    'objectid' => $orderid]));
+                    'objectid' => $orderid,
+                    'itemid' => $itemid,
+                    'component' => $component,
+                    'paymentarea' => $paymentarea]));
             $event->trigger();
         }
 
