4df082df902e0ee98a2512270a2b01bb9f8cd2f6
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2022-06-01 11:50:32 +0200

    Version 0.0.2 (2022060100)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = 'v0.0.1';
-$plugin->version   = 2021051701;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 'v0.0.2';
+$plugin->version   = 2022060100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051100;        // Requires this Moodle version.
 $plugin->component = 'paygw_payunity';       // Full name of the plugin (used for diagnostics).
