ba78d40728122e10dbed10a10b983de1f72175e1
Andrii Semenets <semteacher@gmail.com>
2024-07-24 15:44:36 +0300

    Moodle 4.4: EN Translation sorted

--- lang/en/paygw_payone.php
+++ lang/en/paygw_payone.php
@@ -22,49 +22,43 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['EPS'] = "EP";
+$string['MASTER'] = "MC";
+$string['VISA'] = "VC";
 $string['amountmismatch'] = 'The amount you attempted to pay does not match the required fee. Your account has not been debited.';
 $string['authorising'] = 'Authorising the payment. Please wait...';
 $string['brandname'] = 'Brand name';
 $string['brandname_help'] = 'An optional label that overrides the business name for the payone account on the payone site.';
 $string['cannotfetchorderdetails'] = 'Could not fetch payment details from payone. Your account has not been debited.';
+$string['checkout'] = 'Checkout';
 $string['clientid'] = 'Client ID';
 $string['clientid_help'] = 'The client ID that payone generated for your application.';
+$string['delivery_error'] = 'Your payment was successful, but there was an error during delivery. Please contact support.';
 $string['environment'] = 'Environment';
 $string['environment_help'] = 'You can set this to Sandbox if you are using sandbox accounts (for testing purpose only).';
+$string['error'] = 'Error';
 $string['gatewaydescription'] = 'payone is an authorised payment gateway provider for processing credit card transactions.';
 $string['gatewayname'] = 'payone';
 $string['internalerror'] = 'An internal error has occurred. Please contact us.';
 $string['live'] = 'Live';
+$string['loading'] = 'Loading...';
+$string['more'] = "More";
+$string['other_options'] = "Other payment options";
+$string['paycredit'] = "Pay with creditcard";
+$string['payment_added'] = 'Payment transaction was started. (Open order was added.)';
+$string['payment_alreadyexists'] = 'Payment not treated because it already exists.';
+$string['payment_completed'] = 'Payment transaction was completed.';
+$string['payment_error'] = 'An error occured during the payment. Please try again later.';
+$string['payment_successful'] = 'Payment successful. Click to continue to your course.';
 $string['paymentnotcleared'] = 'payment not cleared by payone.';
 $string['pluginname'] = 'payone';
 $string['pluginname_desc'] = 'The payone plugin allows you to receive payments via payone.';
 $string['privacy:metadata'] = 'The payone plugin does not store any personal data.';
+$string['proceed'] = 'Proceed';
+$string['quick_checkout'] = "Quick checkout";
 $string['repeatedorder'] = 'This order has already been processed earlier.';
 $string['sandbox'] = 'Sandbox';
 $string['secret'] = 'Secret';
 $string['secret_help'] = 'The secret that payone generated for your application.';
-
-$string['checkout'] = 'Checkout';
-$string['loading'] = 'Loading...';
-
-$string['payment_added'] = 'Payment transaction was started. (Open order was added.)';
-$string['payment_completed'] = 'Payment transaction was completed.';
-$string['payment_successful'] = 'Payment successful. Click to continue to your course.';
-$string['payment_error'] = 'An error occured during the payment. Please try again later.';
-$string['payment_alreadyexists'] = 'Payment not treated because it already exists.';
-$string['delivery_error'] = 'Your payment was successful, but there was an error during delivery. Please contact support.';
-
 $string['success'] = 'Success';
-$string['error'] = 'Error';
-$string['proceed'] = 'Proceed';
-
-$string['other_options'] = "Other payment options";
-$string['more'] = "More";
-
-$string['quick_checkout'] = "Quick checkout";
-$string['paycredit'] = "Pay with creditcard";
-
 $string['unknownbrand'] = "UK";
-$string['MASTER'] = "MC";
-$string['VISA'] = "VC";
-$string['EPS'] = "EP";
