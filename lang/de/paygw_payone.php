2c04d22798fcacfa5592405bcebb175aff134777
Andrii Semenets <semteacher@gmail.com>
2024-07-24 15:46:25 +0300

    Moodle 4.4: DE Translation sorted

--- lang/de/paygw_payone.php
+++ lang/de/paygw_payone.php
@@ -22,66 +22,58 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['1'] = "Visa";
+$string['117'] = "Maestro";
+$string['132'] = "Diners Club";
+$string['2'] = "American Express";
+$string['3'] = "Mastercard";
+$string['302'] = "Apple Pay";
+$string['3124'] = "P24";
+$string['320'] = "Google Pay";
+$string['3306'] = "Klarna";
+$string['5404'] = "WeChat Pay";
+$string['5405'] = "Alipay";
+$string['5406'] = "EPS";
+$string['5408'] = "Bank transfer by Worldline";
+$string['809'] = "iDEAL";
+$string['840'] = "Paypal";
+$string['EPS'] = "EP";
+$string['MASTER'] = "MC";
+$string['VISA'] = "VC";
 $string['amountmismatch'] = 'Der Betrag, den Sie zu zahlen versuchen, entspricht nicht dem gespeicherten Betrag. Ihr Konto wurde nicht belastet.';
 $string['authorising'] = 'Authorisiere die Zahlung. Bitte warten...';
 $string['brandname'] = 'Markenname';
 $string['brandname_help'] = 'Ein optionaler Name der den Namen Payiunity auf der Zahlungsseite ersetzt.';
 $string['cannotfetchorderdetails'] = 'Konnte keine Zahlungsdetails von payone erhalten. Ihr Konto wurde nicht belastet.';
+$string['checkout'] = 'Checkout';
 $string['clientid'] = 'Kunden ID';
 $string['clientid_help'] = 'Die Kunden ID die payone für Ihre Seite generiert hat.';
+$string['delivery_error'] = 'Die Zahlung war erfolgreich, aber bei der Auslieferung gab es ein Problem. Bitte wenden Sie sich an den Support.';
 $string['environment'] = 'Umgebung.';
 $string['environment_help'] = 'You can set this to Sandbox if you are using sandbox accounts (for testing purpose only).';
+$string['error'] = 'Fehler';
 $string['gatewaydescription'] = 'payone ist ein authorisierter Zahlungsanbieter um Ihre Kreditkartenzahlung abzuwickeln.';
 $string['gatewayname'] = 'payone';
 $string['internalerror'] = 'Ein interner Fehler ist aufgetreten. Bitte kontaktieren Sie uns.';
 $string['live'] = 'Live';
+$string['loading'] = 'Laden...';
+$string['more'] = "Mehr";
+$string['other_options'] = "Andere Zahlungsarten";
+$string['paycredit'] = "Mit Kreditkarte zahlen";
+$string['payment_added'] = 'Zahlungstransaktion wurde gestartet. (Offener Auftrag wurde angelegt.)';
+$string['payment_alreadyexists'] = 'Zahlung nicht bearbeitet, da sie bereits existiert.';
+$string['payment_completed'] = 'Zahlungstransaktion wurde abgeschlossen.';
+$string['payment_error'] = 'Ein Fehler ist bei der Zahlung mit payone aufgetreten. Bitte versuchen sie es später erneut.';
+$string['payment_successful'] = 'Zahlung erfolgreich. Klicken sie auf "Fortfahren" um zu Ihrem Kurs weitergeleitet zu werden.';
 $string['paymentnotcleared'] = 'Zahlung wurde von payone nicht akzeptiert.';
 $string['pluginname'] = 'payone';
 $string['pluginname_desc'] = 'Das payone plugin erlaubt es, Zahlungen mittels payone zu erhalten.';
 $string['privacy:metadata'] = 'Das payone plugin speichert keine persönlichen Daten.';
+$string['proceed'] = 'Fortfahren';
+$string['quick_checkout'] = "Schneller Checkout";
 $string['repeatedorder'] = 'Der Auftrag wurde bereits früher bearbeitet.';
 $string['sandbox'] = 'Sandbox';
 $string['secret'] = 'Access Token';
 $string['secret_help'] = 'Der access Token den payone für diese Anwendung bereitstellt.';
-
-$string['checkout'] = 'Checkout';
-$string['loading'] = 'Laden...';
-
 $string['success'] = 'Erfolg';
-$string['error'] = 'Fehler';
-$string['proceed'] = 'Fortfahren';
-
-$string['payment_added'] = 'Zahlungstransaktion wurde gestartet. (Offener Auftrag wurde angelegt.)';
-$string['payment_completed'] = 'Zahlungstransaktion wurde abgeschlossen.';
-$string['payment_successful'] = 'Zahlung erfolgreich. Klicken sie auf "Fortfahren" um zu Ihrem Kurs weitergeleitet zu werden.';
-$string['payment_error'] = 'Ein Fehler ist bei der Zahlung mit payone aufgetreten. Bitte versuchen sie es später erneut.';
-$string['payment_alreadyexists'] = 'Zahlung nicht bearbeitet, da sie bereits existiert.';
-$string['delivery_error'] = 'Die Zahlung war erfolgreich, aber bei der Auslieferung gab es ein Problem. Bitte wenden Sie sich an den Support.';
-
-$string['other_options'] = "Andere Zahlungsarten";
-$string['more'] = "Mehr";
-
-$string['quick_checkout'] = "Schneller Checkout";
-$string['paycredit'] = "Mit Kreditkarte zahlen";
-
 $string['unknownbrand'] = "UK";
-$string['MASTER'] = "MC";
-$string['VISA'] = "VC";
-$string['EPS'] = "EP";
-
-
-$string['5405'] = "Alipay";
-$string['2'] = "American Express";
-$string['302'] = "Apple Pay";
-$string['5408'] = "Bank transfer by Worldline";
-$string['132'] = "Diners Club";
-$string['5406'] = "EPS";
-$string['320'] = "Google Pay";
-$string['809'] = "iDEAL";
-$string['3306'] = "Klarna";
-$string['117'] = "Maestro";
-$string['3'] = "Mastercard";
-$string['3124'] = "P24";
-$string['840'] = "Paypal";
-$string['1'] = "Visa";
-$string['5404'] = "WeChat Pay";
